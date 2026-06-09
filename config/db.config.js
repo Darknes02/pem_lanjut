d5437331b8dfd2cb644089ecc5bb4ec83df5bd8c
Darknes02 <nevianevi24@gmail.com>
2026-06-09 16:45:52 +0700

    Editing the db config file

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,29 @@
  import { DataTypes, Sequelize } from "sequelize";
  import mysql from "mysql2"
- const db = new Sequelize('bukumahapro', 'root', '', {
-     host: "localhost",
+ const dbname = "web_lanjut";
+
+ const DB_NAME = process.env.DB_NAME || dbname;
+ const DB_USER = process.env.DB_USER || 'avnadmin';
+ const DB_PASSWORD = process.env.DB_PASSWORD;
+ const DB_HOST = process.env.DB_HOST;
+ const DB_PORT = process.env.DB_PORT;
+
+ const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+     host: DB_HOST,
      dialect: "mysql",
+     port: DB_PORT,
      dialectModule: mysql,
+     dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+     },
       "define": {
         "timestamps": false
       }
     });
 export default db;
 
-(async()=>{
+/*(async()=>{
 await db.sync();
-})();
+})();*/
