7d9050890f9b8ee148c2d220f4dcc36f65e3e487
Darknes02 <nevianevi24@gmail.com>
2026-06-24 10:54:23 +0700

    updateUser

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -31,10 +31,10 @@ export const deleteUser = async (req, res) => {
   try {
     await Users.destroy({
       where: {
-        kode_prodi: req.params.kode_prodi,
+        kode_user: req.params.kode_prodi,
       },
     });
-    res.json({ message: "Data Prodi berhasil dihapus" });
+    res.json({ message: "Data User berhasil dihapus" });
   } catch (error) {
     res.json({ message: error.message });
   }
