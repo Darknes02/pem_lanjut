1710ee50284c8b4de9c6e0bbe8b359faf560b03b
Darknes02 <nevianevi24@gmail.com>
2026-06-24 08:40:42 +0700

    update Auntentifikasi

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import pinjams from "./routes/pinjams.routes.js";
 import detail_pinjam from "./routes/detail_pinjams.routes.js"; //semua import disini diambil dari routes
 import User from "./routes/user.routes.js";
+import {authenticateToken} from "./middleware/verifytokens.js";
 
 const app=express();
 try {
@@ -19,9 +20,11 @@ try {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/user', User);
+app.use(authenticateToken);
 app.use('/api/buku',bukus);
 app.use('/api/mahasiswa',mahasiswas);
-app.use('/api/prodi',prodis);
+app.use('/api/prodi', prodis);
 app.use('/api/pinjam',pinjams);
 app.use('/api/detail_pinjam',detail_pinjam); //warna biru tua tulisan disamakan dengan atas
 app.use('/api/user', User);
